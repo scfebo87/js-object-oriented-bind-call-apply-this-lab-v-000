449dd6838f54524965d5b0ef48bd574e824d0c19
Sean Christopher Febo <scf@techie.com>
2018-07-19 04:14:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-//Your code here
+function justInvoke(fn) { 
+  const result = fn(); 
+  return result; 
+}
+
+function setThisWithCall(fn, thisValue, arg) { 
+  const result = fn.call(thisValue, arg); 
+  return result; 
+}
+  
+function setThisWithApply(fn, thisvalue, arg) {
+  const result = fn.apply(thisvalue, arg);
+  return result;
+}
+
+function returnNewFunctionOf(fn, thisValue) { 
+  const copy = fn.bind(thisValue); 
+  return copy;
+}
