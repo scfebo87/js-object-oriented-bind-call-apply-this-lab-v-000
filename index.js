63cb7fc1f23a40f2d389d40089a9d33a24ab6c25
Sean Christopher Febo <scf@techie.com>
2018-07-19 03:06:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-//Your code here
+function justInvoke(fn) { 
+  const result = fn(); 
+  return result; 
+}
+
+function setThisWithCall(fn, thisValue, arg) { 
+  const result = fn.call(thisValue, arg); 
+  return result; 
+}
+  
+function setThisWithApply(fn)
